29482a7bacb8602e0f44b4f00e0bcc241b25f117
ICEIAM <dmitriy.lukkin@gmail.com>
2022-04-17 22:58:45 +0300

    3. Массивы и модели. [#174892]

---
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -0,0 +1,33 @@
+package ru.job4j.pojo;
+
+public class Library {
+    public static void main(String[] args) {
+        Book cleanCode = new Book("Clean code", 19);
+        Book crimeAndPunishment = new Book("Преступление и наказание", 400);
+        Book whiteNights = new Book("Белые ночи", 250);
+        Book warAndPeace = new Book("Война и Мир", 4000);
+        Book[] shelf = new Book[4];
+        shelf[0] = cleanCode;
+        shelf[1] = crimeAndPunishment;
+        shelf[2] = whiteNights;
+        shelf[3] = warAndPeace;
+        for (int index = 0; index < shelf.length; index++) {
+            Book wholeShelf = shelf[index];
+            System.out.println(wholeShelf.getName() + " - " + wholeShelf.getPages());
+        }
+        for (int index = 0; index < shelf.length; index++) {
+            Book temp = shelf[0];
+            shelf[0] = shelf[3];
+            Book wholeShelf = shelf[index];
+            shelf[3] = temp;
+            System.out.println(wholeShelf.getName() + " - " + wholeShelf.getPages());
+        }
+
+        for (int index = 0; index < shelf.length; index++) {
+            Book wholeShelf = shelf[index];
+            if (wholeShelf.getName() == "Clean code") {
+                System.out.println(wholeShelf.getName() + " - " + wholeShelf.getPages());
+            }
+        }
+    }
+    }
