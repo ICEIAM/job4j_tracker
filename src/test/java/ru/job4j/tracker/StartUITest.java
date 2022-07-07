ae3d59303cae08511dfc8552954617f9b6e9924e
ICEIAM <dmitriy.lukkin@gmail.com>
2022-07-07 17:15:55 +0300

    5.1. Тестирование. Подготовка данных. [#182960 #319607]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -36,8 +36,7 @@ public class StartUITest {
         Tracker tracker = new Tracker();
         Item item = new Item("1");
         tracker.add(item);
-        String[] answers = {String.valueOf(item.getId()),
-                "deleted item"};
+        String[] answers = {String.valueOf(item.getId())};
         StartUI.deleteItem(new StubInput(answers), tracker);
         Item deleted = tracker.findById(item.getId());
         assertNull(deleted);
