3163e862a0baa25d23ec0ceff5877c2d9a5c1368
ICEIAM <dmitriy.lukkin@gmail.com>
2022-07-20 17:23:26 +0300

    0.4. Пользовательские исключения. [#219367 #323199]

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -2,11 +2,13 @@ package ru.job4j.ex;
 
 public class FindEl {
     public static int indexOf(String[] value, String key) throws ElementNotFoundException {
-        int rsl = 0;
+        int rsl = -1;
         for (int i = 0; i < value.length; i++) {
             if (value[i].equals(key)) {
                 rsl = i;
-            } else if (rsl == -1) {
+                break;
+            }
+            if (rsl == -1) {
                 throw new ElementNotFoundException();
             }
         }
