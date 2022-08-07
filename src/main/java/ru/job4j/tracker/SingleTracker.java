025d2f7ed302c988fd351d8be491517f8c5ca2c9
ICEIAM <dmitriy.lukkin@gmail.com>
2022-08-07 03:46:33 +0300

    1. Singleton [#94606]

--- src/main/java/ru/job4j/tracker/SingleTracker.java
+++ src/main/java/ru/job4j/tracker/SingleTracker.java
@@ -5,6 +5,9 @@ public final class SingleTracker {
 
     private Tracker tracker = new Tracker();
 
+    private SingleTracker() {
+    }
+
     public static SingleTracker getInstance() {
         if (instance == null) {
             instance = new SingleTracker();
