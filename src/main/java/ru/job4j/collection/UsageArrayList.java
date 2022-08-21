df52f5f3a2cbfd8c57b051041360ee5fa9a697ef
ICEIAM <dmitriy.lukkin@gmail.com>
2022-08-22 02:58:08 +0300

    0. Коллекции, ArrayList, List, Обобщения [#10094 #334698]

--- src/main/java/ru/job4j/collection/UsageArrayList.java
+++ src/main/java/ru/job4j/collection/UsageArrayList.java
@@ -8,5 +8,8 @@ public class UsageArrayList {
         names.add("Petr");
         names.add("Ivan");
         names.add("Stepan");
+        for (String name : names) {
+            System.out.println(name);
+        }
     }
 }
