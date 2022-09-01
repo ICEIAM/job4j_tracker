3045a08d0255358fa01d33e60ed73abac31c1195
ICEIAM <dmitriy.lukkin@gmail.com>
2022-09-01 16:43:12 +0300

    0. Map, HashMap [#212652 #338313]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -2,13 +2,15 @@ package ru.job4j.collection;
 
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 
 public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> name = new HashMap<>();
         name.put("dmitriy.lukkin@gmail", "Lukin Dmitriy Dmitrievich");
-        for (String key : name.keySet()) {
-            String value = name.get(key);
+        for (Map.Entry<String, String> entry : name.entrySet()) {
+            String key = entry.getKey();
+            String value = entry.getValue();
             System.out.println(key + " = " + value);
         }
     }
