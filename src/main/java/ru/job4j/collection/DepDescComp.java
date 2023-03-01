a88282b42d89b22c395df67064551aa27b179c46
ICEIAM <dmitriy.lukkin@gmail.com>
2023-03-01 23:19:58 +0300

    3. Отсортировать департаменты [#34352]

---
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -0,0 +1,12 @@
+package ru.job4j.collection;
+import java.util.Comparator;
+
+public class DepDescComp implements Comparator<String> {
+    @Override
+    public int compare(String o1, String o2) {
+        String[] first = o1.split("/", 2);
+        String[] second = o2.split("/", 2);
+        int comp = second[0].compareTo(first[0]);
+        return comp == 0 ? o1.compareTo(o2) : comp;
+    }
+}
